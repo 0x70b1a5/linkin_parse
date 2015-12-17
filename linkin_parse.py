bf8e42d8ddd1df64cde2254ac8b1e95e6e354c8a
Tobias Merkle <tgamerkle@gmail.com>
2015-12-16 21:41:35 -0800

    Outputs now written to file. Also, improved error handling

--- linkin_parse.py
+++ linkin_parse.py
@@ -1,5 +1,5 @@
 # so titled because I've always despised the series of phonemes that make up the atrocious word 'LinkdIn' *shudder*
-# and also, avoiding SEO. :D 
+# and also, avoiding SEO. 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
@@ -14,20 +14,21 @@ def getCurJob(soup):
 	try: 
 		p = soup.find_all('p', class_='title', dir='ltr')[0]
 	except (IndexError, AttributeError):
-		print "AAAAAAAAAAAA"
-		pdb.set_trace()
+		print "Failed to acquire target's current job."
 	return tx(p)
 
 def getPastJobs(soup):
 	pastjobs_a = soup.find_all('header')
-	print decode(BeautifulSoup(pastjobs_a.contents).prettify())
+	past_jobs_html = decode(BeautifulSoup(pastjobs_a.contents).prettify())
+	return past_jobs_html
 
-# <my own shamelessly reused code from glvssdoor-scrvper>
+# <shamelessly reused code from glvssdoor-scrvper>
 def decode(thing):
 	try:
-		return u''.join(thing).encode(u'utf-8').strip()
+		thing = u''.join(thing).encode(u'utf-8').strip()
 	except (UnicodeDecodeError, UnicodeEncodeError, TypeError):
-		return thing
+		print "Tried and failed to decode {}".format(thing)
+	return thing
 
 def tx(t):
 	try:
@@ -35,7 +36,7 @@ def tx(t):
 		return x
 	except (AttributeError, IndexError):
 		return t
-# </my own shamelessly reused code from glvssdoor-scrvper>
+# </shamelessly reused code from glvssdoor-scrvper>
 
 if __name__ == '__main__':
 
@@ -75,36 +76,39 @@ if __name__ == '__main__':
 	# earl = 'http://l{0}nked{0}n.com/{0}n/franceshaugen'.format('i') # breaking this up --> Interested Parties can't search their own company name on GitHub and find this script
 
 	# for now, read from file instead
-	f=os.getcwdu().append('/page.html')
-	file = open(f,'w')
+	f=os.getcwdu()+'/page.html'
+	soup = BeautifulSoup(open(f,'w'), 'html.parser')
 
 	# get page source:
 	print 'Acquiring sauce.'
 
-	# initialize webdriver
-	ff = webdriver.Firefox()
+	# # initialize webdriver
+	# ff = webdriver.Firefox()
 
-	# log in to the dummy account
-	ff.get('http://l{0}nked{0}n.com/uas/log{0}n'.format('i'))
-	lie = ff.find_element_by_id("session_key-login")
-	lie.send_keys('cron.jobber@yandex.com')
-	time.sleep(1)
-	fib = ff.find_element_by_id("session_password-login")
-	fib.send_keys('passpass\r')
-	time.sleep(5)
+	# # log in to the dummy account
+	# ff.get('http://l{0}nked{0}n.com/uas/log{0}n'.format('i'))
+	# lie = ff.find_element_by_id("session_key-login")
+	# lie.send_keys('cron.jobber@yandex.com')
+	# time.sleep(1)
+	# fib = ff.find_element_by_id("session_password-login")
+	# fib.send_keys('passpass\r')
+	# time.sleep(5)
 
-	# get the goods from Earl
-	ff.get(earl)
-	time.sleep(3)
+	# # get the goods from Earl
+	# ff.get(earl)
+	# time.sleep(3)
 
-	# get the source
-	sauce = decode(ff.page_source)
-	ff.close()
+	# # get the source
+	# sauce = decode(ff.page_source)
+	# ff.close()
 	
 	# make some soup with the delicious sauce
 	print 'Brewing delicious soup.'
-	soup = BeautifulSoup(sauce, 'html.parser')
+	# soup = BeautifulSoup(sauce, 'html.parser')
 	# get our hands dirty
 	print 'Entering the Matrix.'
-	getJobs(soup)
 	pdb.set_trace()
+
+	with open(os.getcwdu()+"/results.txt", 'w') as out_f:
+		file.write(getCurJob(soup))
+		file.write(getPastJobs(soup))
